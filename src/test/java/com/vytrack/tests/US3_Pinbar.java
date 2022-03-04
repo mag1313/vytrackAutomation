3cfd69246b0a015c8417e6b90c452ca7b6779954
AdultJedi <93745774+AdultJedi@users.noreply.github.com>
2022-03-04 10:36:56 -0500

    Done with first part of Task 3

--- src/test/java/com/vytrack/tests/US3_Pinbar.java
+++ src/test/java/com/vytrack/tests/US3_Pinbar.java
@@ -1,8 +1,40 @@
 package com.vytrack.tests;
 
+import com.vytrack.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class US3_Pinbar {
-    public static void main(String[] args) {
+    WebDriver driver;
+    WebElement verify;
+    @BeforeMethod
+    public void setUpMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void deskTopOptionTest(){
+        driver.get("https://app.vytrack.com/user/login");
+        WebElement inputUserName = driver.findElement(By.xpath("//*[@id=\"prependedInput\"]"));
+        inputUserName.sendKeys("user166");
+        WebElement inputPassword = driver.findElement(By.xpath("//*[@id=\"prependedInput2\"]"));
+        inputPassword.sendKeys("UserUser123");
+
+       WebElement login = driver.findElement(By.xpath("//*[@id=\"_submit\"]"));
+
+       login.click();
+
+       WebElement learnHowToBar = driver.findElement(By.xpath("//*[@id=\"pinbar\"]/div[1]/div/a"));
+
+       learnHowToBar.click();
 
     }
+
 }
-// Josh G Project
