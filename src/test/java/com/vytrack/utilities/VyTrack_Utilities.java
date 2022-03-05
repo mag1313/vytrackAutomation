daaf02d97b2e69223074ad56564cd1255d18a19f
mag1313 <maglovanyi13@gmail.com>
2022-03-05 11:34:52 -0500

    vytrack login

--- src/test/java/com/vytrack/utilities/VyTrack_Utilities.java
+++ src/test/java/com/vytrack/utilities/VyTrack_Utilities.java
@@ -8,15 +8,22 @@ public class VyTrack_Utilities {
 
     public static void login(String username, String password) {
 
+        Driver.getDriver().get(ConfigurationReader.getProperty("env2"));
+        Driver.getDriver().findElement(By.id("prependedInput")).sendKeys(username);
+        Driver.getDriver().findElement(By.cssSelector("#prependedInput")).sendKeys(password);
+        Driver.getDriver().findElement(By.tagName("button")).click();
+
     }
 
-    public static void loginAsDriver(){
+    public static void loginAsDriver() {
 
 
     }
 
-    public static void loginAsStoreManager(){}
+    public static void loginAsStoreManager() {
+    }
 
-    public static void loginAsSalesManager(){}
+    public static void loginAsSalesManager() {
+    }
 
 }
