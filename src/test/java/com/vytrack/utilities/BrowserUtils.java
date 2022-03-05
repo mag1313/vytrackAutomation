5278ee25ac401062908d34d0239e9e35678adc16
mag1313 <maglovanyi13@gmail.com>
2022-03-05 16:04:35 -0500

    US84

--- src/test/java/com/vytrack/utilities/BrowserUtils.java
+++ src/test/java/com/vytrack/utilities/BrowserUtils.java
@@ -6,7 +6,10 @@ import org.testng.Assert;
 import java.util.Set;
 
 public class BrowserUtils {
-
+    /*
+        This method will accept int (in seconds) and execute Thread.sleep
+        for given duration
+         */
     public static void sleep(int second){
         second *=1000;
         try {
@@ -16,6 +19,14 @@ public class BrowserUtils {
         }
     }
 
+
+    /*
+    This method accepts 3 arguments.
+    Arg1: webdriver
+    Arg2: expectedInUrl : for verify if the url contains given String.
+        - If condition matches, will break loop.
+    Arg3: expectedInTitle to be compared against actualTitle
+     */
     public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedInTitle){
 
         Set<String> allWindowsHandles = driver.getWindowHandles();
@@ -36,10 +47,11 @@ public class BrowserUtils {
         Assert.assertTrue(actualTitle.contains(expectedInTitle));
     }
 
+    /*
+    This method accepts a String "expectedTitle" and Asserts if it is true
+     */
+
     public static void verifyTitle(WebDriver driver ,String expectedTitle){
-
         Assert.assertEquals(driver.getTitle(), expectedTitle);
-
     }
-
 }
