c48820e6a786c8db31c5f2f1b4269461aed487bd
mag1313 <maglovanyi13@gmail.com>
2022-03-05 11:23:02 -0500

    vytrack.utilities

--- src/test/java/com/vytrack/utilities/VyTrack_Utilities.java
+++ src/test/java/com/vytrack/utilities/VyTrack_Utilities.java
@@ -6,35 +6,17 @@ import org.openqa.selenium.WebElement;
 
 public class VyTrack_Utilities {
 
-        //WE HAVE TO CREATE OUR CUSTOM LOGIN METHOD FOR THIS PROJECT!
-
-    /*
-    public static void vy_track_login(WebDriver driver) {
-        //3. Enter valid username
-        WebElement inputUsername = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
-        inputUsername.sendKeys("helpdesk1@cybertekschool.com");
-
-        //4. Enter valid password
-        WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
-        inputPassword.sendKeys("UserUser");
-        //5. Click to Log In button
-        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
-        loginButton.click();
+    public static void login(String username, String password) {
 
     }
 
-    public static void crm_login(WebDriver driver, String username, String password) {
-        //3. Enter valid username
-        WebElement inputUsername = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
-        inputUsername.sendKeys(username);
-
-        //4. Enter valid password
-        WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
-        inputPassword.sendKeys(password);
-        //5. Click to Log In button
-        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
-        loginButton.click();
+    public static void loginAsDriver(){
 
 
-     */
+    }
+
+    public static void loginAsStoreManager(){}
+
+    public static void loginAsSalesManager(){}
+
 }
