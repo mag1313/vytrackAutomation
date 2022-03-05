2741117f667a20fa1601fade2397b1621e83b5bf
mag1313 <maglovanyi13@gmail.com>
2022-03-05 16:07:49 -0500

    US84

--- src/test/java/com/vytrack/tests/US84_Jack.java
+++ src/test/java/com/vytrack/tests/US84_Jack.java
@@ -58,4 +58,86 @@ public class US84_Jack {
         Assert.assertEquals(actualErrorMessage99, expectedErrorMessage99);
 
     }
+
+    @Test
+    public void verifying_errorMessage_salesManager() {
+        VyTrack_Utilities.loginAsSalesManager();
+
+        //Step1 = Login Hover Over the "Activities" -> click on "Calendar Events"
+        WebElement activities = driver.findElement(By.xpath("//*[@id='main-menu']/ul/li[3]/a/span"));
+
+        //Creating object of an Actions class
+        Actions actions = new Actions(driver);
+
+        //Performing the mouse hover action on the target element.
+        actions.moveToElement(activities).perform();
+
+        WebElement calendarEvents = Driver.getDriver().findElement(By.xpath("//*[@id='main-menu']/ul/li[3]/div/div/ul/li[3]/a/span"));
+        actions.moveToElement(calendarEvents);
+        actions.click();
+
+        //2.Click on "Create Calendar"
+        //Creat Calendar
+        WebElement createCalendar = Driver.getDriver().findElement(By.xpath("//a[@title='Create Calendar event']"));
+        createCalendar.click();
+
+        //3. Check the "Repeat" checkBox
+        //Repeat checkBox
+        WebElement repeat = Driver.getDriver().findElement(By.xpath("//input[@id='recurrence-repeat-view375']"));
+        repeat.click();
+
+        //Error Message Less Than 1.
+        WebElement ValueLessThan1 = Driver.getDriver().findElement(By.xpath("//span//span[.='The value have not to be less than 1.']/span"));
+        String actualErrorMessage1 = ValueLessThan1.getText();
+        String expectedErrorMessage1 = "The value have not to be less than 1.";
+        Assert.assertEquals(actualErrorMessage1, expectedErrorMessage1);
+
+        //Error Message More Than 99.
+        WebElement ValueMoreThan99 = Driver.getDriver().findElement(By.xpath("//span//span[.='The value have not to be more than 99.']/span"));
+        String actualErrorMessage99 = ValueLessThan1.getText();
+        String expectedErrorMessage99 = "The value have not to be more than 99.";
+        Assert.assertEquals(actualErrorMessage99, expectedErrorMessage99);
+
+    }
+
+    @Test
+    public void verifying_errorMessage_storeManager() {
+        VyTrack_Utilities.loginAsStoreManger();
+
+        //Step1 = Login Hover Over the "Activities" -> click on "Calendar Events"
+        WebElement activities = driver.findElement(By.xpath("//*[@id='main-menu']/ul/li[3]/a/span"));
+
+        //Creating object of an Actions class
+        Actions actions = new Actions(driver);
+
+        //Performing the mouse hover action on the target element.
+        actions.moveToElement(activities).perform();
+
+        WebElement calendarEvents = Driver.getDriver().findElement(By.xpath("//*[@id='main-menu']/ul/li[3]/div/div/ul/li[3]/a/span"));
+        actions.moveToElement(calendarEvents);
+        actions.click();
+
+        //2.Click on "Create Calendar"
+        //Creat Calendar
+        WebElement createCalendar = Driver.getDriver().findElement(By.xpath("//a[@title='Create Calendar event']"));
+        createCalendar.click();
+
+        //3. Check the "Repeat" checkBox
+        //Repeat checkBox
+        WebElement repeat = Driver.getDriver().findElement(By.xpath("//input[@id='recurrence-repeat-view375']"));
+        repeat.click();
+
+        //Error Message Less Than 1.
+        WebElement ValueLessThan1 = Driver.getDriver().findElement(By.xpath("//span//span[.='The value have not to be less than 1.']/span"));
+        String actualErrorMessage1 = ValueLessThan1.getText();
+        String expectedErrorMessage1 = "The value have not to be less than 1.";
+        Assert.assertEquals(actualErrorMessage1, expectedErrorMessage1);
+
+        //Error Message More Than 99.
+        WebElement ValueMoreThan99 = Driver.getDriver().findElement(By.xpath("//span//span[.='The value have not to be more than 99.']/span"));
+        String actualErrorMessage99 = ValueLessThan1.getText();
+        String expectedErrorMessage99 = "The value have not to be more than 99.";
+        Assert.assertEquals(actualErrorMessage99, expectedErrorMessage99);
+
+    }
 }
