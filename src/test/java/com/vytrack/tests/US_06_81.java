2dabd988b29282b19d291164a311fd196295021c
arstanbektemirov <abektemirov82@gmail.com>
2022-03-07 08:44:35 -0500

    US-06(23-81)

--- src/test/java/com/vytrack/tests/US_06_81.java
+++ src/test/java/com/vytrack/tests/US_06_81.java
@@ -1,10 +1,9 @@
 package com.vytrack.tests;
 
 import com.vytrack.utilities.Driver;
-import org.openqa.selenium.By;
-import org.openqa.selenium.ElementClickInterceptedException;
-import org.openqa.selenium.ElementNotInteractableException;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -18,14 +17,10 @@ import static com.vytrack.utilities.utilities_for_login.*;
 public class US_06_81 {
 
     List<String> expectedDotText=new ArrayList<>(Arrays.asList("view", "edit", "delete"));
-    @BeforeMethod
-    public void setUp() {
 
-
-    }
 
     @Test
-    public void verify() {
+    public void verifyUser() {
         for (String username : userList) {
             Driver.getDriver();
             Driver.openUrl("https://qa1.vytrack.com/user/login");
@@ -33,17 +28,16 @@ public class US_06_81 {
             Driver.getDriver().findElement(By.xpath("//div[@id]/ul/li[1]/a/span")).click();
             Driver.getDriver().findElement(By.xpath("//span[.='Vehicles']")).click();
             List<WebElement> links = Driver.getDriver().findElements(By.xpath("//tr/td[20]/div/div/a"));
-            int count=0;
             for (WebElement driver : links) {
             driver.click();
-           }
-            List<WebElement> dots=Driver.getDriver().findElements(By.xpath("//*[@id]/div/div//div/div/ul/li/ul/li/a"));
+            }
+            int count=0;
+           List<WebElement> dots=Driver.getDriver().findElements(By.xpath("//*[@id]/div/div//div/div/ul/li/ul/li/a"));
                 for (WebElement eachDot : dots) {
                     count++;
                     System.out.println(count+":-"+eachDot.getAttribute("title"));
                 }
-            Assert.assertEquals((links.size()*3),dots.size());
-            System.out.println(links.size());
+            Assert.assertEquals((links.size()* expectedDotText.size()),dots.size());
             Driver.close();
                 }
             }
@@ -52,29 +46,59 @@ public class US_06_81 {
 
 
     @Test
-    public void verify2() {
+    public void verifySalesManager() {
         for (String username : salesmanager) {
             Driver.getDriver();
             Driver.openUrl("https://qa1.vytrack.com/user/login");
             crm_login(Driver.getDriver(), username, password);
-           try{
+
+            try{
                Driver.getDriver().findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[2]")).click();
            }catch (ElementClickInterceptedException e){
                Driver.getDriver().findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[2]")).click();
            }catch (ElementNotInteractableException e){}
             Driver.getDriver().findElement(By.xpath("//span[.='Vehicles']")).click();
+
             List<WebElement> links = Driver.getDriver().findElements(By.xpath("//tr/td[20]/div/div/a"));
-            int count = 0;
             for (WebElement driver : links) {
-                count++;
-                System.out.println(count + ": " + driver.getText());
                 driver.click();
-                List<WebElement> dots = Driver.getDriver().findElements(By.xpath("//tr[1]/td[20]//li/a"));
-                for (WebElement eachDot : dots) {
-                    count++;
-                    System.out.println(count + ": " + eachDot.getAttribute("title"));
-                }
             }
+            List<WebElement> dots=Driver.getDriver().findElements(By.xpath("//*[@id]/div/div//div/div/ul/li/ul/li/a"));
+            int count=0;
+            for (WebElement eachDot : dots) {
+                count++;
+                System.out.println(count+":-"+eachDot.getAttribute("title"));
+            }
+            Assert.assertEquals((links.size()* expectedDotText.size()),dots.size());
+            Driver.close();
+        }
+    }
+
+    @Test
+    public void verifyStoreManager() {
+        for (String username : storemanger) {
+            Driver.getDriver();
+            Driver.openUrl("https://qa1.vytrack.com/user/login");
+            crm_login(Driver.getDriver(), username, password);
+            try{
+                Driver.getDriver().findElement(By.xpath("//*[@id]/ul/li[2]/a/span")).click();
+            }catch (ElementClickInterceptedException e){
+                Driver.getDriver().findElement(By.xpath("//*[@id]/ul/li[2]/a/span")).click();
+            }catch (ElementNotInteractableException e){}
+            Driver.getDriver().findElement(By.xpath("//span[.='Vehicles']")).click();
+
+                List<WebElement> links = Driver.getDriver().findElements(By.xpath("//*//tr/td[21]/div/div/a"));
+                for (WebElement driver : links) {
+                    driver.click();
+                }
+
+            int count=0;
+            List<WebElement> dots=Driver.getDriver().findElements(By.xpath("//*[@id]/div/div//div/div/ul/li/ul/li/a"));
+            for (WebElement eachDot : dots) {
+                count++;
+                System.out.println(count+":-"+eachDot.getAttribute("title"));
+            }
+            Assert.assertEquals((links.size()* expectedDotText.size()),dots.size());
             Driver.close();
         }
     }
