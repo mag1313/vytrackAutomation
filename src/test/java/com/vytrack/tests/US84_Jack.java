ed7af4200186ba16fea141f36385faf6f6df27a3
mag1313 <maglovanyi13@gmail.com>
2022-03-08 07:53:18 -0500

    US84

--- src/test/java/com/vytrack/tests/US84_Jack.java
+++ src/test/java/com/vytrack/tests/US84_Jack.java
@@ -95,6 +95,36 @@ public class US84_Jack extends TestBase {
         WebElement calendarEvent = Driver.getDriver().findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[5]/div/div/ul/li[4]/a/span"));
         calendarEvent.click();
 
+        //2.Click on "Create Calendar"
+        //Creat Calendar
+        WebElement createCalendar = Driver.getDriver().findElement(By.xpath("//a[@title='Create Calendar event']"));
+        createCalendar.click();
+
+        //3. Check the "Repeat" checkBox
+        //Repeat checkBox
+        WebElement repeat = Driver.getDriver().findElement(By.xpath("//*[@id='recurrence-repeat-view430']"));
+        repeat.click();
+
+        //Error Message Less Than 1.
+        WebElement inputBox1 = Driver.getDriver().findElement(By.xpath("//input[@name='temp-validation-name-454']"));
+        inputBox1.sendKeys("0" + Keys.ENTER);
+        WebElement ValueLessThan1 = Driver.getDriver().findElement(By.xpath("//span//span[.='The value have not to be less than 1.']/span"));
+
+        String actualErrorMessage1 = ValueLessThan1.getText();
+        String expectedErrorMessage1 = "The value have not to be less than 1.";
+        Assert.assertEquals(actualErrorMessage1, expectedErrorMessage1);
+/*
+        //Error Message More Than 99.
+        WebElement repeatEvery99 = Driver.getDriver().findElement(By.xpath("//input[@name='temp-validation-name-454']"));
+        repeatEvery99.sendKeys("100" + Keys.ENTER);
+        WebElement ValueMoreThan99 = Driver.getDriver().findElement(By.xpath("//span//span[.='The value have not to be more than 99.']/span"));
+
+        String actualErrorMessage99 = ValueLessThan1.getText();
+        String expectedErrorMessage99 = "The value have not to be more than 99.";
+        Assert.assertEquals(actualErrorMessage99, expectedErrorMessage99);
+
+
+ */
     }
 
 }
