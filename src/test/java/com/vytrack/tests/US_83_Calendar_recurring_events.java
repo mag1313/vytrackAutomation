31165bef293b01b050e09468393fa42d379b2091
Kaloyan <kkaloyan@yahoo.com>
2022-03-09 22:40:51 -0500

    branch for US 83 created

---
+++ src/test/java/com/vytrack/tests/US_83_Calendar_recurring_events.java
@@ -0,0 +1,17 @@
+package com.vytrack.tests;
+
+import org.testng.annotations.Test;
+
+public class US_83_Calendar_recurring_events {
+
+    @Test
+    public void recurringEvents_tc1(){
+
+    }
+
+    @Test
+    public void recurringEvents_tc2(){
+
+    }
+
+}
