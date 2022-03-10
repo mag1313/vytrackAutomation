a965a3bd6fdc4faf348de9a92e8af4443bdec8da
cristinamartinescu <martikristi92@yahoo.com>
2022-03-10 16:33:08 -0500

    US85_Description

---
+++ src/test/java/com/vytrack/tests/US85_Cristina_Description.java
@@ -0,0 +1,6 @@
+package com.vytrack.tests;
+
+public class US85_Cristina_Description {
+    // US85_Description
+
+}
--- src/test/java/com/vytrack/tests/base/US85_Cristina_Description.java
+++
@@ -1,5 +0,0 @@
-package com.vytrack.tests.base;
-
-public class US85_Cristina_Description {
-    // US85
-}
