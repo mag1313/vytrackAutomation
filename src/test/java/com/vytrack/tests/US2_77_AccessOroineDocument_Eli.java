5626898fdc8f83d89dfe7f796363a4e3d3f93126
Eli <eli.j.gotay@gmail.com>
2022-03-15 13:52:37 -0400

    Updated US77

--- src/test/java/com/vytrack/tests/US2_77_AccessOroineDocument_Eli.java
+++ src/test/java/com/vytrack/tests/US2_77_AccessOroineDocument_Eli.java
@@ -1,8 +1,35 @@
 package com.vytrack.tests;
 
-public class US2_77_AccessOroineDocument_Eli {
+import com.vytrack.tests.base.TestBase;
+import com.vytrack.utilities.VyTrack_Utilities;
+import org.testng.annotations.Test;
+
+public class US2_77_AccessOroineDocument_Eli extends TestBase {
+
+    @Test
+    public void verifyAccessAsSalesManager(){
+        VyTrack_Utilities.loginAsSalesManager();
+
+    }
+
+    @Test
+    public void verifyAccessAsStoreManager(){
+        VyTrack_Utilities.loginAsSalesManager();
+
+    }
+
+    @Test
+    public void verifyAccessAsDriver(){
+        VyTrack_Utilities.loginAsDriver();
+
+    }
 
 }
+
+//User Story: As a user, I should be able to access the Oroinc Documentation page.
+
+//AC #1: Users access the Oronic Documentation page by clicking the question icon. The page URL: https://doc.oroinc.com/
+
 
 /*
 USERNAME:
