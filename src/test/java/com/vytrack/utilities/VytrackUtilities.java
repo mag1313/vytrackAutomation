463cc55af0b40d07a6d091603785bdd2ef52e501
vlatkapersin <vlatka.persin@gmail.com>
2022-03-19 10:11:02 -0400

    Commit US86 initial

--- src/test/java/com/vytrack/utilities/VytrackUtilities.java
+++ src/test/java/com/vytrack/utilities/VytrackUtilities.java
@@ -1,6 +1,9 @@
 package com.vytrack.utilities;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class VytrackUtilities {
     public static void login(String username, String password) {
@@ -50,5 +53,15 @@ public class VytrackUtilities {
         Driver.getDriver().findElement(By.cssSelector("#prependedInput2")).sendKeys(ConfigurationReader.getProperty("sales_manager_password"));
         //click login button
         Driver.getDriver().findElement(By.tagName("button")).click();
+
+        public static void waitTillLoaderMaskDisappear () {
+            try {
+                WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 20);
+                WebElement loaderMask = Driver.getDriver().findElement(By.cssSelector("div[class='loader-mask shown']"));
+                wait.until(ExpectedConditions.invisibilityOf(loaderMask));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
